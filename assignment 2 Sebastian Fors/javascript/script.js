fd08aa3429df93b9793e4bc35698b9f05057262c
Alrik He <alrik_he@hotmail.com>
2024-10-30 13:27:52 +0100

    feedback

--- assignment 2 Sebastian Fors/javascript/script.js
+++ assignment 2 Sebastian Fors/javascript/script.js
@@ -1,11 +1,20 @@
-let OX = "";
-for (let index = 0; index < 10; index++) {
-    if (index === 4) {
-        OX = OX + "Q";
-    } else if (index % 2) {
-        OX = OX + "X";
-    } else {
-        OX = OX + "O";
-    }
-}
+let OX = ""; //namn på variabel är flummig, småbokstäver i början
+
+for (let i = 0; i < 10; i++)  // ändra index till  i
+    if (i === 4) //curlybracets ta bort semicolon
+        OX += "Q"   //OX = OX + "Q"; samma += ökar
+    else if (i % 2) 
+        OX += "X"
+    else 
+        OX += "O"
+
+//snackar om null
+    
+// index i context med arrayer
+
+// index i template
+/* for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+} */
+
 console.log(OX);
--- assignment 2 Sebastian Fors/javascript/script3.js
+++ assignment 2 Sebastian Fors/javascript/script3.js
@@ -6,6 +6,63 @@ for (let index = 0; index < citynames.length; index++) {
         console.log(element[inner]);
     }
 }
+
+for (let i = 0; i < citynames.length; i++) {
+    const city = citynames[i]
+    for (let j = 0; j < city.length; j++) {
+       if(j==0)
+        console.log(city.charAt(j).toUpperCase());
+       else 
+        console.log(city.charAt(j));
+    }
+} 
+
+for (let i = 0; i < citynames.length; i++) {
+    citynames[i].charAt(0).toUpperCase() + citynames[i].slice(1)
+        for (let j = 0; j < citynames[i].length; j++) 
+            console.log(citynames[i][j])
+}
+
 function capitalizeFirstLetter(val) {
     return String(val).charAt(0).toUpperCase() + String(val).slice(1);
 }
+
+//Rein ytterberg , kung fu
+//Håkan .... 
+
+
+
+// Object
+
+let array = [ 1 , "hej" ,false , true]
+array[3] = "alrik"
+array.push("alrik") 
+array.unshift("He")  
+let antalDeletedElement = 0
+array.splice(1,antalDeletedElement,"hejsan")
+
+
+//         key: value
+let obj = { 
+    id:1,
+    greeting:"hej",
+    human:true,
+    adress: "yxgatan 5b",
+    log: function (x) {
+        console.log(x)
+    },
+    getAdress: function () { 
+        return this.adress 
+    }
+} 
+
+function log(x) {
+    console.log(x)
+}
+function getAdress() { 
+     return this.adress 
+}
+
+alert()
+console.log(obj.id);
+document.write()
